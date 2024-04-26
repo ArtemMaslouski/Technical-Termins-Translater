28691f596e3ae67e52032eb43960cb7472a15416
Artsiom <masloartem20@gmail.com>
2024-04-26 13:38:39 +0300

    Исправлены баги и добавлен вывод в textarea

--- client.js
+++ client.js
@@ -2,24 +2,29 @@ let translateButton = document.querySelector("#Translate");
 translateButton.addEventListener("click", function () {
   let russianField = document.querySelector("#RussianWord");
   let englishField = document.querySelector("#EnglishWord");
+  let explonationField = document.querySelector("#Explonation");
 
-  if (!englishField.value) {
+  if (!englishField.value && !russianField.value) {
+    alert("Перевод невозможен! Заполните поля!");
+  } else if (!englishField.value) {
     let value = russianField.value;
     fetch(`http://localhost:3000/Russian?RussianWord=${value}`)
       .then((response) => response.json())
       .then((response) => {
-        let { russiantermin, englishtermin } = response[0];
+        let { russiantermin, englishtermin, explonation } = response[0];
         russianField.value = russiantermin;
         englishField.value = englishtermin;
+        explonationField.value = explonation;
       });
   } else {
     let value = englishField.value;
     fetch(`http://localhost:3000/English?EnglishWord=${value}`)
       .then((response) => response.json())
       .then((response) => {
-        let { russiantermin, englishtermin } = response[0];
+        let { russiantermin, englishtermin, explonation } = response[0];
         russianField.value = russiantermin;
         englishField.value = englishtermin;
+        explonationField.value = explonation;
       });
   }
 });
@@ -27,15 +32,19 @@ translateButton.addEventListener("click", function () {
 let russianField = document.querySelector("#RussianWord");
 russianField.addEventListener("click", function () {
   let englishField = document.querySelector("#EnglishWord");
+  let explonationField = document.querySelector("#Explonation");
 
   englishField.value = "";
   russianField.value = "";
+  explonationField.value = "";
 });
 
 let englishField = document.querySelector("#EnglishWord");
 englishField.addEventListener("click", function () {
   let russianField = document.querySelector("#RussianWord");
+  let explonationField = document.querySelector("#Explonation");
 
   englishField.value = "";
   russianField.value = "";
+  explonationField.value = "";
 });
